30e8bd705ca951e74685c7e26e9dcfb7e1b0b1ea
OlgaKruglik <137288699+OlgaKruglik@users.noreply.github.com>
2025-11-10 17:23:32 +0300

    fix: helment

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(helmet({ contentSecurityPolicy: false }));
+// app.use(helmet({ contentSecurityPolicy: false }));
 
 
 const router = express.Router();
